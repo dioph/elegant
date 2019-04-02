d89aa01a600d3bc3afefc55d2ecaea209dbae360
dioph <diofanto.nunes@gmail.com>
2019-04-02 15:44:40 -0300

    cpp file

---
+++ aspy/methods.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+using namespace std;
+
+ gauss_seidel(Y, V0, S, tipos, eps)
+{
+    double delta;
+    V;
+    do
+    {
+        for (int i = 0; i < N; i++)
+        {
+            
+            V[i] = ()/Y[i][i]
+        }
+    } while (delta > eps);
+    
+}
+
+void main ()
+{
+    return;
+}
