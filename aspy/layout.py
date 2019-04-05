641a78829fe5edadc41c3681bb215fc267fbe601
dioph <diofanto.nunes@gmail.com>
2019-04-05 19:51:57 -0300

    update_grid working

--- aspy/layout.py
+++ aspy/layout.py
@@ -1,8 +1,5 @@
-import kivy
-from kivy.uix import *
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 
 
@@ -10,11 +7,11 @@ presentation = Builder.load_file('./interface.kv')
 
 
 class Interface(FloatLayout):
-    def update_grid(self, b11, r_elements):
-        # if elements[0].state == 'down':
-        #     b11.text = elements[0].text
-        # pass
-        print(r_elements.children.state)
+    def update_grid(self, place, r_elements):
+        for child in r_elements.children:
+            if child.state is 'down':
+                place.text = child.text
+                break
 
 
 class InterfaceApp(App):
