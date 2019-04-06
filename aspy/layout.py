0c26e5cee7d79644ea0651ec1a723d0063ff7ef2
fmndantas <44785783+fmndantas@users.noreply.github.com>
2019-04-05 21:09:42 -0300

    - simplification of update grid

--- aspy/layout.py
+++ aspy/layout.py
@@ -2,51 +2,35 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 
 presentation = Builder.load_file('./interface.kv')
 
 
-class Interface(FloatLayout):
-    def variables(self):
-        """
-        Called as a contructor but only for used variables
-        """
-        self.SYM = None
 
-    def add_grid(self, grid):
-        """
-        Initializes the grid
-        """
-        for i in range(0, grid.cols):
-            for j in range(0, grid.rows):
+class Interface(FloatLayout):
+    def add_grid(self, grid, elements):
+        """Initializes the grid"""
+        for i in range(grid.cols):
+            for j in range(grid.rows):
                 grid.add_widget(Button())
 
         for child in grid.children:
-            child.bind(on_press=self.update_grid)  # passes child as an argument
+            child.bind(on_press=lambda x: self.update_grid(x, elements))
+        print('ok')
 
-    def get_pressed_toggle_btn_symbol(self, elements):
-        """
-        Returns the text of the current (pressed) ToggleButton
-        """
-        for e in elements.children:
-            if isinstance(e, Button):
-                if e.state == 'down':
-                    self.SYM = e.text
-                    break
-                else:
-                    self.SYM = ""
-
-    def update_grid(self, child):
-        print(self.SYM)
+    def update_grid(self, child, elements):
         """
         Updates the button icon in the grid
         :param child: the button in the grid to be updated
         """
-        try:
-            if self.SYM is not None:
-                child.text = self.SYM
-        except AttributeError:
-            pass
+        for toggleButton in elements.children:
+            if isinstance(toggleButton, ToggleButton):
+                if toggleButton.state == 'down':
+                    child.text = toggleButton.text
+                    break
+                else:
+                    child.text = ""
 
 
 class InterfaceApp(App):
