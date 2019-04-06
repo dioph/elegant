b1a6745fa3753ca2c1c2042a2bdb9daa8f6ca672
fmndantas <44785783+fmndantas@users.noreply.github.com>
2019-04-06 09:59:08 -0300

    - Newton-Raphson method

--- aspy/layout.py
+++ aspy/layout.py
@@ -2,35 +2,41 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
-from kivy.uix.togglebutton import ToggleButton
 
 presentation = Builder.load_file('./interface.kv')
 
 
-
 class Interface(FloatLayout):
     def add_grid(self, grid, elements):
-        """Initializes the grid"""
+        """Initializes the grid
+
+        Parameters
+        ----------
+        grid: the grid to be initialized
+        elements: the grid of togglebuttons
+        """
         for i in range(grid.cols):
             for j in range(grid.rows):
                 grid.add_widget(Button())
 
-        for child in grid.children:
-            child.bind(on_press=lambda x: self.update_grid(x, elements))
-        print('ok')
+        for square in grid.children:
+            square.bind(on_press=lambda x: self.update_grid(x, elements))
 
-    def update_grid(self, child, elements):
+    def update_grid(self, square, elements):
+        """Updates the button icon in the grid
+
+        Parameters
+        ----------
+        square: the position in the grid to be updated
+        elements: the grid of togglebuttons
         """
-        Updates the button icon in the grid
-        :param child: the button in the grid to be updated
-        """
-        for toggleButton in elements.children:
-            if isinstance(toggleButton, ToggleButton):
-                if toggleButton.state == 'down':
-                    child.text = toggleButton.text
+        for child in elements.children:
+            if isinstance(child, Button):
+                if child.state == 'down':
+                    square.text = child.text
                     break
                 else:
-                    child.text = ""
+                    square.text = ""
 
 
 class InterfaceApp(App):
--- aspy/methods.py
+++ aspy/methods.py
@@ -41,6 +41,77 @@ def gauss_seidel(Y, V0, S, eps=None, Niter=1):
     return V
 
 
-def newton_raphson(Y, X0, S0, eps=None, Niter=1):
-    pass
 
+
+def newton_raphson(Y, V0, S, eps=None, Niter=1):
+    """Newthon-Raphson Method
+    Parameters
+    ----------
+    Y: Matriz de admitâncias
+    V0: Vetor de chutes iniciais (N,1)
+    S0: Potencias aparantes especificadas (N,2)
+    eps:
+    Niter:
+
+    Returns
+    -------
+    V: Aproximacao para tensao nos nós
+    """
+    def Gij(i, j):
+        return np.abs(Y[i][j])*np.cos(np.angle(Y[i][j]))
+
+    def Bij(i, j):
+        return np.abs(Y[i][j])*np.sin(np.angle(Y[i][j]))
+
+    def Pi(i):
+        _ = 0
+        for n in range(N):
+            if n != N:
+                _ += np.abs(V[i]*V[n]*Y[i][n])*np.cos(np.angle(Y[i][n])+np.angle(V[n])-np.angle(V[i]))
+        return np.abs(V[i])^2*Gij(i, i)+_
+
+    def Qi(i):
+        _ = 0
+        for n in range(N):
+            if n != N:
+                _ += np.abs(V[i]*V[n]*Y[i][n])*np.sin(np.angle(Y[i][n])+np.angle(V[n])-np.angle(V[i]))
+        return -np.abs(V[i])^2*Bij(i, i)-_
+
+    def Mii(i):
+        return -Q[i]-np.abs(V[i])**2*Bij(i, i)
+
+    def Nii(i):
+        return Pi(i)-np.abs(V[i])**2*Gij(i, i)
+
+    def Mij(i, j):
+        return np.abs(V[j])*np.abs(V[i]*Y[i][j])*np.sin(np.angle(Y[i][j]) + np.angle(V[j]) - np.angle(V[i]))
+
+    def Nij(i, j):
+        return -np.abs(V[i]*V[j]*Y[i][j]*np.cos(np.angle(Y[i][j]) + np.angle(V[j]) - np.angle(V[i])))
+
+    def J_inv(N):
+        J11, J12, J21, J22 = np.zeros([N-1][N-1])
+        for i in range(2, N):
+            for j in range(2, N):
+                J11[i][j] = Mii(i)
+                J21[i][j] = Nii(i)
+                if i == j:
+                    J12[i][j] = Nii(i)+2*np.abs(V[i])**2*Gij(i, i)
+                    J22[i][j] = -Mii(i)-2*np.abs(V[i])**2*Bij(i, i)
+                else:
+                    J12[i][j] = -Nij(i, j)
+                    J22[i][j] = Mij(i, j)
+
+        J_inv = np.linalg.inv(np.array([J11, J12], [J21, J22]))
+
+        return J_inv
+
+    N = V0.size
+    V = V0.copy()
+    delta = np.inf
+    if eps is None:
+        eps = np.inf
+    count = 0
+    while delta < eps or count < Niter:
+        for i in range(N):
+            pass
