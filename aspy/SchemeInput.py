922b51f5a6dc4d745e1c9e13aeee05173ae4c51f
fmndantas <44785783+fmndantas@users.noreply.github.com>
2019-05-03 13:11:26 -0300

    redesigned interface

--- aspy/SchemeInput.py
+++ aspy/SchemeInput.py
@@ -2,10 +2,10 @@ import sys
 import traceback
 import logging
 
+
 import numpy
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton,\
-    QApplication, QGraphicsView, QGraphicsScene, QGridLayout
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPen, QPixmap, QBrush
 
 from aspy.core import *
@@ -13,20 +13,21 @@ from aspy.core import *
 
 # TODO: circles in grid
 # TODO: Define store method for data in graph
-# TODO: Insert bar, doubleclick - QPixMap?
 # TODO: Insert line only between two bars
+# TODO: On scrolling, delete only the yellow squares
 
 
 class SchemeInputer(QGraphicsScene):
-    def __init__(self, n=25, *args, **kwargs):
+    def __init__(self, n=10, length=50, *args, **kwargs):
         super(SchemeInputer, self).__init__(*args, **kwargs)
         self.n = n
-        self.setSceneRect(0, 0, 50*self.n, 50*self.n)  # Visible portion of Scene to View
-        self.selector_radius = 25.0
-        self._oneUnityLength = 50.0
-        self.quantizedInterface = self.getQuantizedInterface()
+        self._oneUnityLength = length
         self._cursorHistory = numpy.ones((2, 2)) * -1
         self._selectorHistory = numpy.array([None, -1, -1])  # 0: old QRect, 1 & 2: coordinates to new QRect
+        self.SceneView = self.setSceneRect(0, 0, self._oneUnityLength*self.n, self._oneUnityLength*self.n)  # Visible portion of Scene to View
+        self.selector_radius = length/2
+        self.quantizedInterface = self.getQuantizedInterface()
+        self.elementsGrid = numpy.zeros_like(self.quantizedInterface, object)
         self.showQuantizedInterface()
 
 
@@ -60,47 +61,74 @@ class SchemeInputer(QGraphicsScene):
         pen.setColor(Qt.yellow)
         brush = QBrush()
         brush.setColor(Qt.yellow)
-        brush.setStyle(Qt.SolidPattern)
+        brush.setStyle(Qt.Dense7Pattern)
         x, y = coordinates
         QRect = self.addRect(x, y, self._oneUnityLength, self._oneUnityLength, pen, brush)
         return QRect
 
 
     def clearSquare(self, oldQRect):
+        # print('oldRect: ', oldQRect)
+        # TODO: bug in clearing square after scrolling in any direction
         if oldQRect is not None:
             self.removeItem(oldQRect)
 
 
-    def mousePressEvent(self, event):
-        pressed = event.scenePos().x(), event.scenePos().y()
-        for central_point in self.quantizedInterface.flatten():
-            if self.distance(pressed, central_point) <= self.selector_radius:
-                self.clearSquare(self._selectorHistory[0])
-                #  catches up right corner
-                self._selectorHistory[1] = central_point.x() - self._oneUnityLength/2
-                self._selectorHistory[2] = central_point.y() - self._oneUnityLength/2
-                self._selectorHistory[0] = self.drawSquare(self._selectorHistory[1:])
+    def mouseDoubleClickEvent(self, event):
+        try:
+            double_pressed = event.scenePos().x(), event.scenePos().y()
+            for central_point in self.quantizedInterface.flatten():
+                if self.distance(double_pressed, central_point) <= self.selector_radius:
+                    pixmap = QPixmap('./data/buttons/DOT.jpg')
+                    pixmap = pixmap.scaled(self._oneUnityLength, self._oneUnityLength, Qt.KeepAspectRatio)
+                    sceneItem = self.addPixmap(pixmap)
+                    pixmap_coords = central_point.x()-self._oneUnityLength/2, central_point.y()-self._oneUnityLength/2
+                    sceneItem.setPos(pixmap_coords[0], pixmap_coords[1])
+                    # print('added element')
+        except Exception:
+            logging.error(traceback.format_exc())
 
+
+    def mousePressEvent(self, event):
+        # L button: 1; R button: 2
+        print('mouse button: ', event.button())
+        if event.button() in (1, 2):
+            pressed = event.scenePos().x(), event.scenePos().y()
+            for central_point in self.quantizedInterface.flatten():
+                if self.distance(pressed, central_point) <= self.selector_radius:
+                    self.clearSquare(self._selectorHistory[0])
+                    #  catches up right corner
+                    self._selectorHistory[1] = central_point.x() - self._oneUnityLength/2
+                    self._selectorHistory[2] = central_point.y() - self._oneUnityLength/2
+                    self._selectorHistory[0] = self.drawSquare(self._selectorHistory[1:])
+                    # print('currentRect: ', self._selectorHistory[0])
+
+
+    def sceneRectChanged(self, QRectF):
+        pass
 
     def mouseMoveEvent(self, event):
         """Give behavior to wire tool"""
         clicked = event.scenePos().x(), event.scenePos().y()
-        for central_point in self.quantizedInterface.flatten():
-            try:
-                if self.distance(clicked, central_point) <= self.selector_radius:
-                    if numpy.all(self._cursorHistory[0] < 0):  # No source
-                        self._cursorHistory[0, 0] = central_point.x()
-                        self._cursorHistory[0, 1] = central_point.y()
-                    if central_point.x() != self._cursorHistory[0, 0] or central_point.y() != self._cursorHistory[0, 1]:  # Set destiny
-                        self._cursorHistory[1, 0] = central_point.x()
-                        self._cursorHistory[1, 1] = central_point.y()
-                    if (numpy.all(self._cursorHistory > 0)) and (numpy.any(self._cursorHistory[0, :] != numpy.any(self._cursorHistory[1, :]))):
-                        self.drawLine(self._cursorHistory)  # Draw the line
-                        for i in range(numpy.shape(self._cursorHistory)[0]):  # Reset _historyCursor
-                            for j in range(numpy.shape(self._cursorHistory)[1]):
-                                self._cursorHistory[i, j] = -1
-            except Exception as e:
-                logging.error(traceback.format_exc())
+        # print(event.button())
+        if event.button() == 0:
+            for central_point in self.quantizedInterface.flatten():
+                try:
+                    if self.distance(clicked, central_point) <= self.selector_radius:
+                        if numpy.all(self._cursorHistory[0] < 0):  # No source
+                            self._cursorHistory[0, 0] = central_point.x()
+                            self._cursorHistory[0, 1] = central_point.y()
+                        if central_point.x() != self._cursorHistory[0, 0] or central_point.y() != self._cursorHistory[0, 1]:  # Set destiny
+                            self._cursorHistory[1, 0] = central_point.x()
+                            self._cursorHistory[1, 1] = central_point.y()
+                        if (numpy.all(self._cursorHistory > 0)) and (numpy.any(self._cursorHistory[0, :] != numpy.any(self._cursorHistory[1, :]))):
+                            # print('DEBUG >>> scrolling is drawing lines. _cursorHistory: ', self._cursorHistory)
+                            self.drawLine(self._cursorHistory)  # Draw the line
+                            for i in range(numpy.shape(self._cursorHistory)[0]):  # Reset _historyCursor
+                                for j in range(numpy.shape(self._cursorHistory)[1]):
+                                    self._cursorHistory[i, j] = -1
+                except Exception:
+                    logging.error(traceback.format_exc())
 
 
     def getQuantizedInterface(self):
@@ -132,44 +160,108 @@ class SchemeInputer(QGraphicsScene):
         pass
 
 
-    def on_changed(self):
-        """Updates grid spacing when window has been resized"""
-        pass
-
 
 class CircuitInputer(QWidget):
-    def __init__(self, Viewer, parent=None):
+    def __init__(self, parent=None):
         super(CircuitInputer, self).__init__(parent)
-        self.Viewer = Viewer  # SchemeInput Viewer
+        self.scene = SchemeInputer()
+        self.view = QGraphicsView(self.scene)
         self.SchemeInputLayout = QHBoxLayout()  # Layout for SchemeInput
-        self.SchemeInputLayout.addWidget(Viewer)
+        self.SchemeInputLayout.addWidget(self.view)
 
-        self.ElementSelectorButtonsLayout = QGridLayout()  # Layout for ToggleButtons
-        self.PushButtonTrafo = QPushButton('Trafo')
-        self.PushButtonLT = QPushButton('Linha de transmissão')
-        self.PushButtonPV = QPushButton('Barra PV')
-        self.PushButtonPQ = QPushButton('Barra PQ')
-        self.ElementSelectorButtonsLayout.addWidget(self.PushButtonTrafo, 1, 1)
-        self.ElementSelectorButtonsLayout.addWidget(self.PushButtonLT, 2, 1)
-        self.ElementSelectorButtonsLayout.addWidget(self.PushButtonPV, 3, 1)
-        self.ElementSelectorButtonsLayout.addWidget(self.PushButtonPQ, 4, 1)
+        # self.ElementSelectorButtonsLayout = QGridLayout()  # Layout for ToggleButtons
+        # self.PushButtonTrafo = QPushButton('Trafo')
+        # self.PushButtonLT = QPushButton('Linha de transmissão')
+        # self.PushButtonBarra = QPushButton('Barra')
+        # # self.PushButtonPQ = QPushButton('Barra PQ')
+        # self.ElementSelectorButtonsLayout.addWidget(self.PushButtonTrafo, 1, 1)
+        # self.ElementSelectorButtonsLayout.addWidget(self.PushButtonLT, 2, 1)
+        # self.ElementSelectorButtonsLayout.addWidget(self.PushButtonBarra, 3, 1)
+        # self.ElementSelectorButtonsLayout.addWidget(self.PushButtonPQ, 4, 1)
 
-        self.ToggleButtonsContainer = QVBoxLayout()
-        self.ToggleButtonsContainer.addStretch()
-        self.ToggleButtonsContainer.addLayout(self.ElementSelectorButtonsLayout)
-        self.ToggleButtonsContainer.addStretch()
+        ### Inspector ###
+        self.InspectorLayout = QVBoxLayout()  # Inspector
+        self.TypeLayout = QHBoxLayout()
+        # self.isBar = QRadioButton('Bar'); self.isLT = QRadioButton('Lt'); self.isTrafo = QRadioButton('Trafo')
+        # self.isBar.toggled.connect(lambda: self.defineElementType(self.isBar))
+        # self.isLT.toggled.connect(lambda: self.defineElementType(self.isLT))
+        # self.isTrafo.toggled.connect(lambda: self.defineElementType(self.isTrafo))
+        self.TypeLayout.addStretch()
+        # self.TypeLayout.addWidget(self.isBar); self.TypeLayout.addWidget(self.isLT); self.TypeLayout.addWidget(self.isTrafo)
+        self.TypeLayout.addStretch()
+        self.InspectorLayout.addLayout(self.TypeLayout)
 
+        ### General Layout for general bar case ###
+        self.BarLayout = QVBoxLayout()
+        self.BarName = QLabel('Bar title')
+        self.BarName.setAlignment(Qt.AlignCenter)
+        self.BarV = QLabel('|V| = 1.0')
+        self.BarV.setAlignment(Qt.AlignCenter)
+        self.BarAngle = QLabel('\u03b4 = 45º')
+        self.BarAngle.setAlignment(Qt.AlignCenter)
+        self.AddGenerationLabel = QLabel('Geração')
+        self.AddGenerationLabel.setAlignment(Qt.AlignCenter)
+        self.AddGenerationButton = QPushButton('+')
+        self.AddGenerationButton.pressed.connect(self.addGeneration)  # Bind button to make input editable
+        self.AddGenerationFormLayout = QFormLayout()
+        self.AddLoadFormLayout = QFormLayout()
+        self.PgInput = QLineEdit()
+        self.QgInput = QLineEdit()
+        self.PgInput.setEnabled(False); self.QgInput.setEnabled(False)
+        self.AddGenerationFormLayout.addRow('Qg', self.QgInput)
+        self.AddGenerationFormLayout.addRow('Pg', self.PgInput)
+
+        self.AddLoadLabel = QLabel('Carga')
+        self.AddLoadLabel.setAlignment(Qt.AlignCenter)
+        self.AddLoadButton = QPushButton('+')
+        self.AddLoadButton.pressed.connect(self.addLoad)
+        self.QlInput = QLineEdit()
+        self.PlInput = QLineEdit()
+        self.PlInput.setEnabled(False); self.QlInput.setEnabled(False)
+        self.AddLoadFormLayout.addRow('Ql ', self.QlInput)
+        self.AddLoadFormLayout.addRow('Pl ', self.PlInput)
+
+        self.BarLayout.addWidget(self.BarName)
+        self.BarLayout.addWidget(self.BarV)
+        self.BarLayout.addWidget(self.BarAngle)
+        self.BarLayout.addWidget(self.AddGenerationLabel)
+        self.BarLayout.addWidget(self.AddGenerationButton)
+        self.BarLayout.addLayout(self.AddGenerationFormLayout)
+        self.BarLayout.addWidget(self.AddLoadLabel)
+        self.BarLayout.addWidget(self.AddLoadButton)
+        self.BarLayout.addLayout(self.AddLoadFormLayout)
+        self.BarLayout.addWidget(self.AddLoadLabel)
+        self.BarLayout.addWidget(self.AddLoadButton)
+
+        ### General Layout for LT case ###
+        self.LtLayout = QVBoxLayout()
+
+        ### Layout that holds Inspector and Stretches ###
+        self.InspectorAreaLayout = QVBoxLayout()
+        self.InspectorAreaLayout.addStretch()
+        self.InspectorAreaLayout.addLayout(self.InspectorLayout)
+        self.InspectorAreaLayout.addLayout(self.BarLayout)
+        self.InspectorAreaLayout.addLayout(self.LtLayout)
+        self.InspectorAreaLayout.addStretch()
+
+        ### Toplayout ###
         self.TopLayout = QHBoxLayout()
-        self.TopLayout.addLayout(self.ToggleButtonsContainer)
+        self.TopLayout.addLayout(self.InspectorAreaLayout)
         self.TopLayout.addLayout(self.SchemeInputLayout)
 
         self.setLayout(self.TopLayout)
 
+    def addGeneration(self):
+        self.PgInput.setEnabled(True)
+        self.QgInput.setEnabled(True)
+
+    def addLoad(self):
+        self.PlInput.setEnabled(True)
+        self.QlInput.setEnabled(True)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    scene = SchemeInputer()
-    view = QGraphicsView(scene)
-    ex = CircuitInputer(view)
+    ex = CircuitInputer()
     ex.show()
     sys.exit(app.exec_())
