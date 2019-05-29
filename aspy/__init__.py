ea6293f740eb57b516252c5f5a461eacfad728f6
fmndantas <fernandodantas1996@gmail.com>
2019-05-29 12:14:43 -0300

    __init__.py creates sessions dir

--- aspy/__init__.py
+++ aspy/__init__.py
@@ -1,3 +1,13 @@
 from .core import *
+import sys, os
+
+if sys.platform in ('win32', 'win64'):
+    home_dir = os.getenv('userprofile')
+    sessions_dir = os.path.join(home_dir, 'Documents\\aspy')
+    if not os.path.exists(sessions_dir):
+        os.mkdir(sessions_dir)
+    else:
+        pass
+
 name = "aspy"
 __version__ = "1.0b1"
