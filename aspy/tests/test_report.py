3a1af43514018efc734f1d984acb8dfd94072d08
dioph <diofanto.nunes@gmail.com>
2019-06-24 11:51:05 -0300

    included data/testdb test case

--- aspy/tests/test_report.py
+++ aspy/tests/test_report.py
@@ -1,9 +1,9 @@
 import shelve
+
 from ..SchemeInput import createLocalData
 from ..report import *
-import numpy as np
 
-with shelve.open('/home/alpaca/aspy/db') as db:
+with shelve.open('aspy/data/testdb') as db:
     tipos, linhas, barras, trafos, grid = createLocalData(db)
     linhas = np.array(linhas)[:, 0]
     trafos = np.array(trafos)[:, 0]
