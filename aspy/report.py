ed14089ccc2ee819ef1c40a866e08ec006539f79
fmndantas <fernandodantas1996@gmail.com>
2019-07-01 18:16:05 -0300

    make_system_schematic

--- aspy/report.py
+++ aspy/report.py
@@ -1,9 +1,11 @@
 import datetime
+import os
 import tempfile
 
+import matplotlib.pyplot as plt
 import numpy as np
 from pylatex import Document, Section, Command, Tabular, Table, NoEscape, \
-    Subsection, MultiColumn, MultiRow, UnsafeCommand
+    Subsection, MultiColumn, MultiRow, UnsafeCommand, Figure
 from pylatex.base_classes import CommandBase
 
 
@@ -23,14 +25,132 @@ def get_scheme(tr):
     return NoEscape('{} {}'.format(code[tr.primary], code[tr.secondary]))
 
 
-def create_report(barras, linhas, trafos, grid):
-    from aspy.SchemeInput import getSessionsDir
+def matplotlib_coordpairs(entity, sfactor=2):
+    coords = np.array(entity[2])
+    x, y = list(coords[:, i] for i in range(np.shape(coords)[1] - 1, -1, -1))
+    return sfactor * x, sfactor * -y
 
+
+def draw_rep_buses(ax, buses, size=250, sfactor=2):
+    for bus in buses:
+        x, y = bus.__getattribute__('posicao')[::-1]
+        plt.scatter(sfactor * x, sfactor * -y, s=size, c='k', zorder=3)
+        ax.annotate(bus.barra_id,
+                    xy=(sfactor * x, sfactor * -y),
+                    color='w',
+                    fontfamily='monospace',
+                    horizontalalignment='center',
+                    verticalalignment='center')
+
+
+def collect_line_data(ax, line):
+    x, y = line.get_data()
+    p1x, p2x, p1y, p2y = x[0], x[1], y[0], y[1]
+    pblx, plx, pbly, ply = x[-2], x[-1], y[-2], y[-1]
+    r_p1 = ax.transData.transform_point([p1x, p1y])
+    r_p2 = ax.transData.transform_point([p2x, p2y])
+    r_pbl = ax.transData.transform_point([pblx, pbly])
+    r_pl = ax.transData.transform_point([plx, ply])
+    return r_p1, r_p2, r_pbl, r_pl
+
+
+def get_line_slopes(line_data):
+    r_p1, r_p2, r_pbl, r_pl = line_data
+    start_slope = np.arctan2(r_p2[1] - r_p1[1], r_p2[0] - r_p1[0])
+    end_slope = np.arctan2(r_pl[1] - r_pbl[1], r_pl[0] - r_pbl[0])
+    return np.rad2deg(start_slope), np.rad2deg(end_slope)
+
+
+def draw_rep_lines(ax, lines, linewidth=1):
+    glines = []
+    for line in lines:
+        coords = matplotlib_coordpairs(line)
+        gline = ax.plot(*coords, linewidth=linewidth, color='b')
+        glines.append(gline)
+    return glines
+
+
+def draw_rep_trafos(ax, trafos, linewidth=1):
+    gtrafos = []
+    for trafo in trafos:
+        coords = matplotlib_coordpairs(trafo)
+        gtrafo = ax.plot(*coords, linewidth=linewidth, color='r')
+        gtrafos.append(gtrafo)
+    return gtrafos
+
+
+def draw_rep_scheme(data):
+    buses, lines, trafos = data
+    ax = plt.gca()
+    glines = draw_rep_lines(ax, lines)
+    gtrafos = draw_rep_trafos(ax, trafos)
+    draw_rep_buses(ax, buses)
+    ax.set_axis_off()
+    return glines, gtrafos
+
+
+def annotate_lines_flux_data(ax, glines, lines):
+    for gentty, lentty in zip(glines, lines):
+        gline, line = gentty[0], lentty[0]
+        real_line_data = collect_line_data(ax, gline)
+        sslope, eslope = get_line_slopes(real_line_data)
+        x, y = gline.get_data()
+        dist = 0.6
+        sx, sy = x[0] + dist * np.cos(np.deg2rad(sslope)), y[0] + dist * np.sin(np.deg2rad(sslope))
+        ex, ey = x[-1] - dist * np.cos(np.deg2rad(eslope)), y[-1] - dist * np.sin(np.deg2rad(eslope))
+        common_config = {'fontfamily': 'monospace', 'rotation_mode': 'anchor', 'fontsize': 5}
+        ax.annotate('{:.1f}'.format(line.S1 * 100),
+                    xy=(sx, sy),
+                    horizontalalignment='left',
+                    verticalalignment='bottom',
+                    rotation=sslope,
+                    color='b',
+                    **common_config)
+        ax.annotate("{:.1f}".format(line.S2 * 100),
+                    xy=(ex, ey),
+                    horizontalalignment='right',
+                    verticalalignment='bottom',
+                    rotation=eslope,
+                    color='b',
+                    **common_config)
+
+
+def annotate_trafos_flux_data(ax, gtrafos, trafos):
+    pass
+
+def make_system_schematic(data, sessions_dir, filename, ext='pdf', testing=False):
+    ax = plt.gca()
+    buses, lines, trafos = data
+    glines, gtrafos = draw_rep_scheme(data)
+    annotate_lines_flux_data(ax, glines, lines)
+    annotate_trafos_flux_data(ax, gtrafos, trafos)
+    img = os.path.join(sessions_dir, filename) + '_i.' + ext
+    if not testing:
+        plt.savefig(img)
+        return img
+    plt.show()
+
+
+def create_report(BUSES, LINES, TRANSFORMERS, GRID_BUSES):
+    if len(LINES) > 0:
+        linhas = np.array(LINES)[:, 0]
+    else:
+        linhas = np.array([])
+    if len(TRANSFORMERS) > 0:
+        trafos = np.array(TRANSFORMERS)[:, 0]
+    else:
+        trafos = np.array([])
+    grid = GRID_BUSES
+    barras = BUSES
+    from aspy.SchemeInput import getSessionsDir
+    sessions_dir = getSessionsDir()
+    filename = next(tempfile._get_candidate_names())
     geometry_options = {"tmargin": "2cm", "lmargin": "2cm", "rmargin": "2cm", "bmargin": "2cm"}
     doc = Document(geometry_options=geometry_options)
     doc.preamble.append(Command('usepackage', 'cmbright'))
     doc.preamble.append(Command('usepackage', 'tikz'))
     doc.preamble.append(Command('usepackage', 'amsmath'))
+    doc.preamble.append(Command('usepackage', 'graphicx'))
     now = datetime.datetime.now()
     doc.append('Relatório gerado automaticamente em '
                '{:02d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d} usando ASPy'.format(
@@ -176,7 +296,5 @@ def create_report(barras, linhas, trafos, grid):
                                  NoEscape('{:.02f}'.format(np.abs(tr.Spu) * 1e8 / tr.snom * 100))),
                                 color=color)
                 tbl.add_hline()
-    filename = next(tempfile._get_candidate_names())
-    sessions_dir = getSessionsDir()
     doc.generate_pdf(sessions_dir + '/' + filename, clean_tex=True)
 
