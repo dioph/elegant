2eb2d4d2e17d51b67a047216c1a59f9ff852cc4a
fmndantas <fernandodantas1996@gmail.com>
2019-07-02 17:41:08 -0300

    no reassignment in variables creation

--- aspy/tests/test_report.py
+++ aspy/tests/test_report.py
@@ -1,13 +1,13 @@
 import shelve
-
 from ..SchemeInput import createLocalData
 from ..report import *
 
-with shelve.open('aspy/data/testdb') as db:
+
+with shelve.open('C:\\Users\\Fernando Dantas\\Documents\\aspy\\db') as db:
     tipos, linhas, barras, trafos, grid = createLocalData(db)
-    linhas = np.array(linhas)[:, 0]
-    trafos = np.array(trafos)[:, 0]
+    isolinhas = np.array(linhas)[:, 0]
+    isotrafos = np.array(trafos)[:, 0]
 
 
 def test_create_report():
-    assert create_report(barras, linhas, trafos, grid)
+    assert create_report(barras, isolinhas, isotrafos, grid)
