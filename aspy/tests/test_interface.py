fe207b2f97372db65c9ee1d59cb3792d0ff6de1e
fmndantas <fernandodantas1996@gmail.com>
2019-07-05 17:31:02 -0300

    interface_automation

--- aspy/tests/test_interface.py
+++ aspy/tests/test_interface.py
@@ -1,90 +1,15 @@
-import pyautogui
+from aspy.SchemeInput import Aspy
+from PyQt5.QtWidgets import QApplication
+import sys
 
-_TRANSLATOR_ = {
-    'l': (-50, None),
-    'u': (None, -50),
-    'r': (50, None),
-    'd': (None, 50),
-    'lu': (-50, -50),
-    'ul': (-50, -50),
-    'ld': (-50, 50),
-    'dl': (-50, 50),
-    'ru': (50, -50),
-    'ur': (50, -50),
-    'rd': (50, 50),
-    'dr': (50, 50)
-}
+app = QApplication(sys.argv)
+aspy = Aspy()
 
-def sequence_to_line(sequence, duration=0.15):
-    commands = sequence.split(' ')
-    pyautogui.mouseDown()
-    for command in commands:
-        if command[0].isdigit():
-            for _ in range(int(command[0])):
-                pyautogui.moveRel(*_TRANSLATOR_[command[1:]], duration=duration)
-        else:
-            pyautogui.moveRel(*_TRANSLATOR_[command], duration=duration)
-    pyautogui.mouseUp()
+def test_buses():
+    pass
+
+sys.exit(app.exec_())
 
 
-def test_interface():
-    # Put bars
-    pyautogui.moveTo(x=317, y=316)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(100, None)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(100, -100)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 200)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(100, -250)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 100)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(100, -350)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 100)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveRel(None, 50)
-    pyautogui.doubleClick()
-    pyautogui.moveTo(317, 316)
-    # Put lines
-    sequence_to_line('2r')
-    sequence_to_line('2ur')
-    sequence_to_line('ur r')
-    top = pyautogui.position()
-    pyautogui.moveRel(-100, 50)
-    sequence_to_line('2r')
-    pyautogui.moveRel(-100, None)
-    sequence_to_line('rd r')
-    pyautogui.moveRel(-200, 50)
-    sequence_to_line('2dr')
-    sequence_to_line('ur r')
-    pyautogui.moveRel(-100, 50)
-    sequence_to_line('2r')
-    pyautogui.moveRel(-100, None)
-    sequence_to_line('dr r')
-    pyautogui.moveRel(-100, -50)
-    sequence_to_line('d dr r')
-    pyautogui.moveTo(*top)
-    for _ in range(8):
-        sequence_to_line('2r')
-        pyautogui.moveRel(-100, 50)
+
+
