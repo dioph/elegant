afae6b544cf4663beafabde8f0bba0f3e673083c
fmndantas <fernandodantas1996@gmail.com>
2019-07-13 12:14:52 -0300

    test_interface

--- aspy/tests/test_interface.py
+++ aspy/tests/test_interface.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from aspy.SchemeInput import Aspy
 from PyQt5.QtWidgets import QApplication
 import sys
@@ -12,4 +13,22 @@ sys.exit(app.exec_())
 
 
 
+=======
+import sys, time
+import unittest
+from unittest import TestCase
+import autoit
+from PyQt5.QtWidgets import QApplication
+
+from aspy.SchemeInput import Aspy, createLocalData
+from aspy.interface_automation import *
+
+
+class test_element_insertion(TestCase):
+    def test_buses_insertion(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+>>>>>>> Stashed changes
 
