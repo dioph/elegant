e0ce57607c875434a45024434787e64679524177
dioph <dioph@protonmail.com>
2020-06-09 19:52:14 -0300

    fix test_report

--- elegant/tests/test_report.py
+++ elegant/tests/test_report.py
@@ -14,7 +14,10 @@ SESSIONS_DIR = getSessionsDir()
 
 
 class ReportTests(unittest.TestCase):
-    def test_report(self):
+    def test_report_was_created(self):
         filename = os.path.join(SESSIONS_DIR, 'report_test.pdf')
-        create_report(system, curves, grid, filename)
-        self.assertTrue(os.path.exists(filename), 'the pdf test file was not successfully generated')
+        new_filename = create_report(system, curves, grid, filename)
+        self.assertTrue(os.path.exists(new_filename), 'the pdf test file was not successfully generated')
+        self.assertEqual(os.path.splitext(filename), os.path.splitext(new_filename))
+        if PDF_SUPPORT:
+            self.assertEqual(filename, new_filename)
