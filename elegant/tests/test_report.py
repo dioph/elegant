67ad74bc81a446cd49f5387a202a27de9fdd0887
dioph <dioph@protonmail.com>
2020-06-15 15:11:28 -0300

    use pytest fixture 'tmpdir' for test_report

--- elegant/tests/test_report.py
+++ elegant/tests/test_report.py
@@ -1,5 +1,3 @@
-import unittest
-
 from elegant.interface import *
 from elegant.report import *
 
@@ -13,11 +11,11 @@ with open(file, 'br') as file:
 SESSIONS_DIR = getSessionsDir()
 
 
-class ReportTests(unittest.TestCase):
-    def test_report_was_created(self):
-        filename = os.path.join(SESSIONS_DIR, 'report_test.pdf')
-        new_filename = create_report(system, curves, grid, filename)
-        self.assertTrue(os.path.exists(new_filename), 'the pdf test file was not successfully generated')
-        self.assertEqual(os.path.splitext(filename), os.path.splitext(new_filename))
-        if PDF_SUPPORT:
-            self.assertEqual(filename, new_filename)
+def test_report_was_created(tmpdir):
+    filename = os.path.join(tmpdir, 'report_test.pdf')
+    new_filename = create_report(system, curves, grid, filename)
+    assert os.path.exists(new_filename), 'the pdf test file was not successfully generated'
+    assert os.path.splitext(filename) == os.path.splitext(new_filename)
+    assert len(tmpdir.listdir()) == 1
+    if PDF_SUPPORT:
+        assert filename == new_filename
